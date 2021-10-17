8a4f74cbcd7c8c2ac8d20874989e652366d2596f
Alexander vargas <alexnadervargasrivera78@gmail.com>
2021-10-16 23:18:23 -0400

    primer parcial

--- PrimerParcial/pregunta1/Rompecabezas.py
+++ PrimerParcial/pregunta1/Rompecabezas.py
@@ -1,6 +1,5 @@
 from busquedas_02 import aestrella, ProblemaBusqueda
 
-<<<<<<< HEAD
 # Elaborar un agente que resuelva un rompecabezas de 4x3  4 filas y 3 colunas
 OBJETIVO = """1-2-3
 4-5-6
@@ -12,27 +11,11 @@ INICIAL = '''e-1-2
 7-a-b'''
 
 
-=======
-#OBJETIVO = """1-2-3-4-a
-#5-6-7-8-b
-#9-c-d-f-g
-#h-i-j-k-l
-#m-n-o-p-e"""
+#INICIAL = """1-2-3
+#4-5-6
+#7-8-9
+#a-e-b"""
 
-#INICIAL = """7-5-3-4-m
-#2-6-1-8-l
-#9-e-j-f-k
-#h-i-d-g-b
-#a-n-o-p-c"""
-# Elaborar un agente que resuelva un rompecabezas de 4x3  4 culunas y 3 filas 
-OBJETIVO = """1-2-3-4
-              5-6-7-8
-              9-a-b-e"""
-
-INICIAL = """e-2-3-b
-             9-a-8-7
-             5-6-4-1"""
->>>>>>> 180c71c41445ebb258e229dcab39f1f3c6426f54
 
 
 def list_to_string(list_):
@@ -49,19 +32,13 @@ def find_location(filas, element_to_find):
     for ir, row in enumerate(filas):
         for ic, element in enumerate(row):
             if element == element_to_find:
-<<<<<<< HEAD
                 #print(element)
-=======
->>>>>>> 180c71c41445ebb258e229dcab39f1f3c6426f54
                 return ir, ic
 
 
 posiciones_objetivo = {}
 filas_objetivo = string_to_list(OBJETIVO)
-<<<<<<< HEAD
 
-=======
->>>>>>> 180c71c41445ebb258e229dcab39f1f3c6426f54
 for numero in "123456789abe":
     posiciones_objetivo[numero] = find_location(filas_objetivo, numero)
 
@@ -75,19 +52,11 @@ class EigthPuzzleProblem(ProblemaBusqueda):
         acciones = []
         if fila_e > 0:
             acciones.append(filas[fila_e - 1][columna_e])
-<<<<<<< HEAD
         if fila_e < 3:
             acciones.append(filas[fila_e + 1][columna_e])
         if columna_e > 0:
             acciones.append(filas[fila_e][columna_e - 1])
         if columna_e < 2:
-=======
-        if fila_e < 2:
-            acciones.append(filas[fila_e + 1][columna_e])
-        if columna_e > 0:
-            acciones.append(filas[fila_e][columna_e - 1])
-        if columna_e < 3:
->>>>>>> 180c71c41445ebb258e229dcab39f1f3c6426f54
             acciones.append(filas[fila_e][columna_e + 1])
 
         return acciones
@@ -119,10 +88,6 @@ class EigthPuzzleProblem(ProblemaBusqueda):
         """
         matriz = string_to_list(estado)
         distancia = 0
-<<<<<<< HEAD
-=======
-
->>>>>>> 180c71c41445ebb258e229dcab39f1f3c6426f54
         for numero in "123456789abe":
             # indice actual
             fila_n, columna_n = find_location(matriz, numero)
